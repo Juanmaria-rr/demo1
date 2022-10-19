ba3cb1343f7e458096dca9d657737479b43b1593
Juanmaria-rr <juanmariaroldanr@gmail.com>
2022-10-19 10:23:08 +0100

    updated py file uniprot slterms

--- src/data_flow/uniprot_location.py
+++ src/data_flow/uniprot_location.py
@@ -14,9 +14,8 @@ df_1=(df
     'Is_a_exploded',
     F.explode_outer(F.split('Is part of',',')).alias('Is_part_exploded'))
 
-## Hacemos un split y select de lo que esta separado por ";"
-# y cogemos el primer elemento de las dos columnas. 
-
+## Make split and select elements seperated by ";"
+# and choose the first element of the two columns
 .withColumn('Is_a_exploded_SL', F.split(F.col('Is_a_exploded'), ';')
 .getItem(0))
 .withColumn('Is_part_SL', F.split(F.col('Is_part_exploded'), ';')
@@ -24,26 +23,22 @@ df_1=(df
 .select(F.col('Name'),F.col('Subcellular location ID').alias('SubcellID'), F.col('Is_part_SL'), F.col('Is_a_exploded_SL'))
 )
 ##### Take parental: terms whose 'Is a" 
-# column is null because they are the main
-parental=(df_1
-.filter(df_1.Is_a_exploded_SL.isNull())
+# column is null because they are the parents
+parental=(df_1.filter(df_1.Is_a_exploded_SL.isNull())
 .withColumnRenamed('Is_a_exploded_SL','Is_a')
 )
-
-child_grouped=(df_1
-.filter(df_1.Is_a_exploded_SL.isNotNull())
+child=df_1.filter(df_1.Is_a_exploded_SL.isNotNull())
+child_grouped=(child
 .select('SubcellID','Is_a_exploded_SL').distinct()
 .groupBy('Is_a_exploded_SL')
 .agg(F.collect_list(F.col('SubcellID')).alias('SubcellID_child'))
 )
-
 parental_child=(parental
 .join(child_grouped, parental.SubcellID == child_grouped.Is_a_exploded_SL, 'left')
 .select('Name','SubcellID','SubcellID_child')
 )
-
 ### Obtention of the list with the hierarchy 
-### and a new cell with all the terms belonging to the name. 
+### and a new column with all the terms belonging to the name. 
 hierarchy=(parental_child
 .select(
     'Name',
